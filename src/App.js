0d6a288961f373945844bc91bb150da0ae7470c2
Dilshod Rahmatov <dilshodrahmatov@gmail.com>
2019-05-10 17:05:44 +0200

    app.js example 2

--- src/App.js
+++ src/App.js
@@ -10,12 +10,21 @@ export default class App extends Component {
     };
   }
 
+  changeStateData = () => {
+    this.setState({
+      userName: this.state.userName === "Dilshod" ? "Bob" : "Dilshod"
+    });
+  };
+
   render() {
     return (
       <div>
         <h4 className="bg-primary text-white text-center p-2">
           {this.state.userName}'s To Do List
         </h4>
+        <button className="btn btn-primary m-2" onClick={this.changeStateData}>
+          Change
+        </button>
       </div>
     );
   }
