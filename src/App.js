0040b8638be1981dee7abbbb8fd5ec488ea63fd5
Dilshod Rahmatov <dilshodrahmatov@gmail.com>
2019-05-10 16:31:22 +0200

    app.js example

--- src/App.js
+++ src/App.js
@@ -3,10 +3,19 @@ import React, { Component } from "react";
 // import './App.css';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: "Dilshod"
+    };
+  }
+
   render() {
     return (
       <div>
-        <h4 className="bg-primary text-white text-center p-2">To Do List</h4>
+        <h4 className="bg-primary text-white text-center p-2">
+          {this.state.userName}'s To Do List
+        </h4>
       </div>
     );
   }
